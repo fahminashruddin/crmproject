2108f32b447c8eb1ddae677d7e37c37479847713
Fahmi Nashruddin <fahminashruddin2005@gmail.com>
2025-12-18 21:06:09 +0700

    fix error export pdf

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index()https://github.com/fahminashruddin/crmproject/pull/7/conflict?name=routes%252Fweb.php&ancestor_oid=3f5402bc381d7ca088be8e8fa29128b36d330eaf&base_oid=ce6843252ddd944a07ac11b60079921489696561&head_oid=e86005b4d0c3a7736a05070639eca7b3a2cba431
+    public function index()
     {
         $user = Auth::user();
 
